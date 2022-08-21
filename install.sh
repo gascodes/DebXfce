271aae753ef5b643b5954a01cfceca08854c40af
Gaston <93546109+gascodes@users.noreply.github.com>
2022-08-21 04:38:37 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 su
-apt-get install xorg xfce4 xfce4-terminal autoconf acountsservice automake curl fakeroot font-terminus gcc git lightdm make mousepad sudo thunar-archive-plugin unzip unrar-free wget xarchiver zsh zsh-autosuggestions zsh-syntax-highlighting zip 
-sudo systemctl enable lightdm.service
+apt-get install xorg xorg-dev xfce4 xfce4-terminal xfce4-dev-tools autoconf acountsservice automake autogen curl fakeroot font-terminus gcc git lightdm make mousepad sudo thunar-archive-plugin unzip unrar-free wget xarchiver zsh zsh-autosuggestions zsh-syntax-highlighting zip libwnck-3-dev libglib2.0-dev libgtk3.0-dev libxfce4ui-2-dev libxfce4panel-2.0-dev
+sudo systemctl enable lightdm.service 
 xdg-user-dirs-update
 bash -c "$(wget -qO - 'https://shlink.makedeb.org/install')"
 chsh -s /bin/zsh
