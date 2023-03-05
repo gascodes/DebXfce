8eb91a4d252103296c1c84bd591e5d6564509b8b
Gaston <93546109+gascodes@users.noreply.github.com>
2023-03-05 15:12:18 -0300

    Update install.sh
    Script to install basic XFCE4 packages in Debian distro

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 su
-apt-get install xorg xorg-dev xfce4 xfce4-terminal xfce4-dev-tools autoconf acountsservice automake autogen curl fakeroot font-terminus gcc git lightdm make mousepad sudo thunar-archive-plugin unzip unrar-free wget xarchiver zsh zsh-autosuggestions zsh-syntax-highlighting zip libwnck-3-dev libglib2.0-dev libgtk3.0-dev libxfce4ui-2-dev libxfce4panel-2.0-dev nala
+apt-get install xorg xorg-dev xfce4 xfce4-terminal xfce4-dev-tools autoconf acountsservice automake autogen curl fakeroot font-terminus gcc git lightdm make mousepad sudo thunar-archive-plugin unzip unrar-free wget xarchiver zsh zsh-autosuggestions zsh-syntax-highlighting zip libwnck-3-dev libglib2.0-dev libgtk3.0-dev libxfce4ui-2-dev libxfce4panel-2.0-dev nala fonts-firacode
 apt install bluez bluez-tools bluetooth blueman
 sudo systemctl enable lightdm.service 
 sudo systemctl enable bluetooth.service 
