8f565ac2553cca8754a719f7a6fde97c729f5c96
Gaston <93546109+gascodes@users.noreply.github.com>
2023-03-05 19:33:33 -0300

    Update installXfce4.sh

--- installXfce4.sh
+++ installXfce4.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
-su
-apt-get install xorg xorg-dev xfce4 xfce4-terminal xfce4-dev-tools autoconf acountsservice automake autogen curl fakeroot font-terminus gcc git lightdm make mousepad sudo thunar-archive-plugin unzip unrar-free wget xarchiver zsh zsh-autosuggestions zsh-syntax-highlighting zip libwnck-3-dev libglib2.0-dev libgtk3.0-dev libxfce4ui-2-dev libxfce4panel-2.0-dev nala fonts-firacode gtk2-engines-murrine
-apt install bluez bluez-tools bluetooth blueman wget libglib2.0-cil-dev golang-gir-gio-2.0-dev libgtk-3-dev libwnck-3-dev libxfce4ui-2-dev libxfce4panel-2.0-dev intltool
+
+sudo sed -i 's/bullseye/bookworm/g' /etc/apt/sources.list
+sudo apt update
+sudo apt upgrade
+sudo apt-get install xorg xorg-dev xfce4 xfce4-terminal xfce4-dev-tools autoconf accountsservice automake autogen curl fakeroot font-terminus gcc git lightdm make mousepad sudo thunar-archive-plugin unzip unrar-free wget xarchiver zsh zsh-autosuggestions zsh-syntax-highlighting zip nala fonts-firacode gtk2-engines-murrine gvfs pulseaudio curl bluez bluez-tools bluetooth blueman wget libglib2.0-cil-dev golang-gir-gio-2.0-dev libgtk-3-dev libwnck-3-dev libxfce4ui-2-dev libxfce4panel-2.0-dev intltool lightdm lightdm-gtk-greeter lightdm-gtk-greeter-settings 
 sudo systemctl enable lightdm.service 
 sudo systemctl enable bluetooth.service 
 xdg-user-dirs-update
