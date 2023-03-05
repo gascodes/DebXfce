fd3fc5d7a37d1e8f70a73ee6c95102ed0afa537c
Gaston <93546109+gascodes@users.noreply.github.com>
2023-03-05 12:05:30 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -5,11 +5,10 @@ apt install bluez bluez-tools bluetooth blueman
 sudo systemctl enable lightdm.service 
 sudo systemctl enable bluetooth.service 
 xdg-user-dirs-update
-chsh -s /bin/zsh
-
-
+#chsh -s /bin/zsh
 
 #Xfce Docklike Plugin
+#Instalamos las despendecias
 sudo apt update && sudo apt upgrade
 sudo apt install wget libglib2.0-cil-dev golang-gir-gio-2.0-dev libgtk-3-dev libwnck-3-dev libxfce4ui-2-dev libxfce4panel-2.0-dev intltool
 wget https://archive.xfce.org/src/panel-plugins/xfce4-docklike-plugin/0.4/xfce4-docklike-plugin-0.4.0.tar.bz2
@@ -17,3 +16,15 @@ tar -xvjf xfce4-docklike-plugin-0.4.0.tar.bz2 && cd xfce4-docklike-plugin-0.4.0
 ./configure
 make
 sudo make install
+
+
+#Tokyo Night Themes & Icons
+# Giteamos los fuentes y copiamos a las carpetas
+sudo apt install gtk2-engines-murrine
+git clone https://github.com/Fausto-Korpsvart/Material-GTK-Themes.git
+cd Material-GTK-Themes
+cd icons
+sudo cp -R .* /usr/share/icons
+cd ..
+cd themes
+sudo cp -R .* /usr/share/themes
